c31b8743378cce31435fa3c2189dbe4ce0ec6c83
Candice <candicejiang1@gmail.com>
2025-05-03 21:31:19 +0100

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoadingIssuesDetailPage = () => {
+  return <div>loading </div>;
+};
+
+export default LoadingIssuesDetailPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -29,7 +29,8 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
