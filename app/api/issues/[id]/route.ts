94dd5ed79a2a6cdb6568b500e0a237a6289325f3
Candice <candicejiang1@gmail.com>
2025-05-06 17:23:34 +0100

    Build an API for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/app/lib/prisma";
+import { issueSchema } from "@/app/validationSchema";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+interface Props {
+    params: Promise<{id: string}>
+}
+export async function PATCH (request: NextRequest, {params}: Props){
+
+ const {id} = await params;
+ const body = await request.json() 
+
+ const validation = issueSchema.safeParse(body);
+ if (!validation.success)
+    return NextResponse.json(validation.error.format(), {status: 400})
+
+ const issue = await prisma.issue.findUnique({where: {id: parseInt(id)}})
+
+ if (!issue)
+    return NextResponse.json({error: 'Invalid issue'}, {status: 404})
+
+ const updatedIssue = await prisma.issue.update({
+    where: {id: issue.id}, 
+    data: {title: body.title, description: body.description},
+
+ });
+ return NextResponse.json(updatedIssue)
+}
--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,14 +1,14 @@
 import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { title } from "process";
-import { createIssueSchema } from "../../validationSchema";
+import { issueSchema } from "../../validationSchema";
 
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
 
-    const validation =  createIssueSchema.safeParse(body)
+    const validation =  issueSchema.safeParse(body)
  
     if(!validation.success)
         return NextResponse.json(validation.error.format(), {status: 400})
