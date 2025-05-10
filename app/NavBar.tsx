cb87d1e431c7ef31047f622fe69ba5afc981a4b4
Candice <candicejiang1@gmail.com>
2025-05-10 19:22:05 +0100

    Remove duplicate skeletons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
+    { label: "Issues", href: "/issues/list" },
   ];
   return (
     <nav className="flex space-x-6 border-b p-5 mb-5 h-16 items-center">
